36b225585e73cc617af27f932421f7751f8584d3
Jean Max <jmaxtb@gmail.com>
2022-11-17 12:28:09 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Attribution from './components/attribution'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <h1>Hello, world</h1>
+      </div>
+      <Attribution/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Attribution.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/Attribution.css"
+
+const Attribution = () => {
+  return (
+    <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Jean Max Trujillo Buendia</a>.
+    </div>
+  )
+}
+
+export default Attribution
